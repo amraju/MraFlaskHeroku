086ec7499ed0dd673ec5b845d258e48571a2abb3
amraju <mraraju@gmail.com>
2022-08-17 07:49:47 +0530

    simple imports

--- sayDash.py
+++ sayDash.py
@@ -1,9 +1,10 @@
 import dash
-import dash_html_components as html
+#import dash_html_components as html
 import plotly.graph_objects as go
-import dash_core_components as dcc
+#import dash_core_components as dcc
 import plotly.express as px
-from dash.dependencies import Input, Output
+#from dash.dependencies import Input, Output
+from dash import html, dcc, Input, Output
 
 
 app = dash.Dash(__name__)
